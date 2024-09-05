5e8144893fe3bf1368fdcbea18cbe9d97b822893
Edeserys <vikinikolov@yahoo.com>
2024-09-05 17:16:39 +0300

    Update mass estimation.py

--- mass estimation.py
+++ mass estimation.py
@@ -29,7 +29,7 @@ max_payload = 9302 # kg
 M = 0.77 
 alt_CR_ft = 35000 # ft
 alt_CR = alt_CR_ft * 0.3048 # m
-dist_TO = 1296 # m
+dist_TO = 1296 # m.
 dist_L =  1210 # m
 Design_R = 2019 * 1000 # m 
 Design_payload = 7200 # kg
@@ -49,9 +49,9 @@ phi = 0.97 # page 106
 psi = 0.0075 # page 106
 E_spec = 44 # MJ/kg
 T_CR = 218.808 # K, ISA 
-a_CR = math.sqrt(gamma*R*T_CR)
-t_E = 30 * 60 # s
-R_div = 300*1000 # m
+a_CR = math.sqrt(gamma * R * T_CR)
+t_E = 20 * 60 # s
+R_div = 300 * 1000 # m
 C_L_CR = 1.7
 C_L_TO = 1.9
 C_L_L = 2.6
@@ -71,14 +71,19 @@ eta_j = V_CR / (TSFC * E_spec)
 
 # Mass estimation:
 R_lost = 1/0.7 * L_over_D_CR * (alt_CR + V_CR**2/(2*g)) # m
-R_nom = Design_R
-R_eq = (R_nom + R_lost) * (1 + f_con) + 1.2 * R_div + t_E * V_CR 
-#f_FuelMass = 1 - euler**(-R_eq/(eta_j * E_spec*10**6/g * L_over_D_CR))
-R_aux = R_eq - R_nom
+R_nom_zero = ZeroPayload_R
+R_nom_design = Design_R
+R_eq_zero = (R_nom_zero + R_lost) * (1 + f_con) + 1.2 * R_div + t_E * V_CR
+R_eq_design = (R_nom_design + R_lost) * (1 + f_con) + 1.2 * R_div + t_E * V_CR
+f_FuelMass = 1 - euler**(-R_eq_design/(eta_j * E_spec*10**6/g * L_over_D_CR))
+print(f_FuelMass)
+R_aux_zero = R_eq_zero - R_nom_zero
+R_aux_design = R_eq_design - R_nom_design
 
 # Masses:
-OE_Mass = maxMTOW_payload * (euler**((max_MTOW_R-R_aux)/(eta_j * L_over_D_CR * E_spec * 10**6/g))-1) / ((euler**((ZeroPayload_R-R_aux)/(eta_j * L_over_D_CR * E_spec * 10**6/g))-euler**((max_MTOW_R-R_aux)/(eta_j * L_over_D_CR * E_spec * 10**6/g))))
-Fuel_Mass = OE_Mass * (euler**((max_MTOW_R-R_aux)/(eta_j * L_over_D_CR * E_spec * 10**6/g))-1) + max_payload*(euler**((max_MTOW_R-R_aux)/(eta_j * L_over_D_CR * E_spec * 10**6/g)))
+OE_Mass = maxMTOW_payload * (euler**((max_MTOW_R+R_aux_design)/(eta_j * L_over_D_CR * E_spec * 10**6/g))-1) / ((euler**((ZeroPayload_R+R_aux_zero)/(eta_j * L_over_D_CR * E_spec * 10**6/g))-euler**((max_MTOW_R+R_aux_design)/(eta_j * L_over_D_CR * E_spec * 10**6/g))))
+Fuel_Mass = OE_Mass * (euler**((max_MTOW_R+R_aux_design)/(eta_j * L_over_D_CR * E_spec * 10**6/g))-1) + max_payload*(euler**((max_MTOW_R+R_aux_design)/(eta_j * L_over_D_CR * E_spec * 10**6/g)))
+#Fuel_Mass = OE_Mass * (euler**((ZeroPayload_R+R_aux)/(eta_j * L_over_D_CR * E_spec * 10**6/g))-1)
 MTOW = maxMTOW_payload + Fuel_Mass + OE_Mass
 
 # Mass Fractions:
@@ -86,4 +91,6 @@ f_FuelMass = Fuel_Mass / MTOW
 f_OEMass = OE_Mass / MTOW
 f_payload = maxMTOW_payload / MTOW
 
-print("Fuel Mass Fraction: " + f"{f_FuelMass:.3f}" + "\nOperation Empty Mass Fraction: " + f"{f_OEMass:.3f}" + "\nPayload Mass Fraction: " + f"{f_payload:.3f}")
+print("Fuel Mass Fraction: " + f"{f_FuelMass:.3f}" + "\nOperational Empty Mass Fraction: " + f"{f_OEMass:.3f}" + "\nPayload Mass Fraction: " + f"{f_payload:.3f}")
+print("\nOperational Empty Weight:", OE_Mass, "kg")
+print("Maximum Take-Off Weight:", MTOW, "kg")
