8226c4997b7c7351bc8b2f9b5c4f4da89f0b2cfd
Daviyoshi <105122603+Daviyoshi@users.noreply.github.com>
2024-09-10 19:31:58 +0200

    finish matching diag

--- assumptions.py
+++ assumptions.py
@@ -1,5 +1,9 @@
+''' Assumptions and other values provided in manual'''
+
 import math
-from constants import pi, euler, gamma, R, g
+from constants import *
+from tlars import *
+
 # Assumptions:
 AR = 10
 WettedR = 6
@@ -9,31 +13,59 @@ CF_Equi = 0.0031 # page 105
 phi = 0.97 # page 106
 psi = 0.0075 # page 106
 E_spec = 44 # MJ/kg
-T_CR = 218.808 # K, ISA 
-a_CR = math.sqrt(gamma * R * T_CR)
+
+
 t_E = 20 * 60 # s
 R_div = 300 * 1000 # m
-C_L_CR = 1.7
-C_L_TO = 1.9
-C_L_L = 2.6
+
+C_LFL = 0.6 # pg 133 landing field length coefficient
+
 
 # Aerodynamic characteristics
 C_D0_CR = WettedR * CF_Equi
 e_CR = 1/(AR * pi * psi + 1/phi)
 L_over_D_CR = 1/2 * math.sqrt(pi * AR * e_CR / C_D0_CR)
+c_dividedby_v = 0.024
 
 # Sea Level
 rho_0 = 1.225 # kg/m^3
 T_0 = 288.15 # K
 p_0 = 101325 # Pa
+
 # Takeoff
-e_TO = e_CR
+delta_f = 35
+
+C_D0_TO = C_D0_CR+0.0013*delta_f
+e_TO = e_CR+.0026*delta_f
+C_L_TO = 1.9
+h2 = 15.24 # m 50ft
+
 rho_TO = rho_0
 T_TO = T_0
 p_TO = p_0
 L_TO = 1296 # m takeoff field length
 
+# Climb
+beta_cl = 0.95
+CR = 7.62
+
+# Cruise
+beta_cr = 0.95
+C_L_CR = 1.7
+T_CR = 218.808 # K, ISA 
+a_CR = math.sqrt(gamma * R * T_CR)
+V_CR = M * a_CR
+rho_CR = 0.3796
+
 # Landing
+delta_f = 20
+
+C_D0_L = C_D0_CR+0.0013*delta_f
+e_L = e_CR+.0026*delta_f
+
+
 landing_field_length= 1210 #m
 rho_L = rho_0
-landing_mass_ratio = 0.7533
+beta_L = 0.7533 # fuel mass fraction - loiter
+C_L_L = 2.6
+V_app = 70
---
+++ matching diagram.py
@@ -0,0 +1,97 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+from assumptions import *
+from constants import *
+from tlars import *
+
+def getAlphaT(WS):
+    #Velocity and mach
+
+    V2 = math.sqrt(WS*2/rho_TO/C_L_TO)
+    M = V2/math.sqrt(gamma*R*T_TO)
+
+    #Local variables
+    Tt = T_TO*(1+(gamma-1)/gamma*M**2)
+    thetaT = Tt/T_0
+
+    pT = p_TO*(1+(gamma-1)/2*M**2)**(gamma/(gamma-1))
+    deltaT = pT/p_0
+    if BR > 0 and BR < 5 and thetaT < 1.07:
+        alphaT= deltaT
+    elif BR >= 5 and BR < 10 and thetaT >= 1.07:
+        alphaT = deltaT*(1-2.1*(thetaT-1.07)/thetaT)
+    elif BR >= 5 and BR <15 and thetaT < 1.07:
+        alphaT = deltaT*(1-(0.43+0.014*BR)*math.sqrt(M))
+    elif BR >= 5 and BR < 15 and thetaT >= 1.07:
+        alphaT = deltaT*(1-(0.43+0.014*BR)*math.sqrt(M)-3*(thetaT-1.07)/(1.5+M))
+    else: print("Error in alphaT, check BR and thetaT")
+
+    return alphaT
+
+loadings = np.linspace(1/1000, 1, 1000)
+loadings_without0 = np.arange(1500, 9000, 100)
+
+
+#Landing limited
+landing_field_length_ws= 1/beta_L*landing_field_length*rho_L*C_L_L/2/C_LFL
+landing_field_length = [landing_field_length_ws for i in range(len(loadings))]
+
+#Approach speed
+speed_approach_ws = (V_app/1.23)**2*rho_L*C_L_L//2
+speeed_approach_list = [speed_approach_ws for i in range(len(loadings))]
+
+#Cruise wing loading
+WS = C_L_CR*0.5*rho_CR*V_CR**2
+cruise_ws = beta_cr/getAlphaT(WS)*(C_D0_CR*0.5*rho_CR*V_CR**2/(beta_cr*WS)+beta_cr*WS/(math.pi*AR*e_CR*0.5*rho_CR*V_CR**2))
+
+cruise = [cruise_ws for i in range(len(loadings_without0))]
+
+#Take off
+kT = 0.85 #Not much clue, should check this out
+takeoff = np.zeros(len(loadings_without0))
+for i in range(len(loadings_without0)):
+    WS = loadings_without0[i]
+    takeoff[i] = 1.15*getAlphaT(WS)*math.sqrt(WS/(L_TO*kT*rho_TO*g*math.pi*AR*e_TO))+4*h2/L_TO
+
+# Climb gradient
+def climbgradient(oswald, beta,engines,C_L):
+    result = np.zeros(len(loadings_without0))
+    for i in range(len(loadings_without0)):
+        WS = loadings_without0[i]
+        V = math.sqrt(WS*2/rho_TO/C_L_TO)
+        result[i] = 2/engines*beta/getAlphaT(WS)*(CR/V+2*C_L/(math.pi*10*oswald))
+    return result
+
+climbgradient_takeoff = climbgradient(oswald=e_TO,beta=beta_cl,engines=2,C_L=C_L_TO)
+climbgradient_takeoff_1OEI = climbgradient(oswald=e_TO,beta=beta_cl,engines=1,C_L=C_L_TO)
+climbgradient_cruise = climbgradient(oswald=e_CR,beta=beta_cr,engines=2,C_L=C_L_CR)
+climbgradient_cruise_1OEI = climbgradient(oswald=e_CR,beta=beta_cr,engines=1,C_L=C_L_CR)
+climbgradient_landing = climbgradient(oswald=e_L,beta=beta_L,engines=2,C_L=C_L_L)
+climbgradient_landing_1OEI = climbgradient(oswald=e_L,beta=beta_L,engines=1,C_L=C_L_L)
+
+# Climb rate
+
+climbrate = np.zeros(len(loadings_without0))
+for i in range(len(loadings_without0)):
+    WS = loadings_without0[i]
+    climbrate[i] = beta_cl/getAlphaT(WS)*math.sqrt((CR**2*rho_TO/(beta_cl*WS*2)*math.sqrt(C_D0_TO*pi*AR*e_TO)+4*C_D0_TO/(pi*AR*e_TO)))
+
+
+#Plot
+plt.title("Matching Diagram")
+plt.xlabel("W/S -[N/m2]")
+plt.ylabel("T/W - [N/N]")
+plt.plot(speeed_approach_list, loadings)
+plt.plot(landing_field_length, loadings )
+plt.plot(loadings_without0,cruise)
+plt.plot(loadings_without0,takeoff)
+plt.plot(loadings_without0,climbgradient_takeoff)
+plt.plot(loadings_without0,climbgradient_takeoff_1OEI)
+plt.plot(loadings_without0,climbgradient_cruise)
+plt.plot(loadings_without0,climbgradient_cruise_1OEI)
+plt.plot(loadings_without0,climbgradient_landing)
+plt.plot(loadings_without0,climbgradient_landing_1OEI)
+plt.plot(loadings_without0,climbrate)
+plt.legend(["Approach Speed", "Landing Field Length", "Cruise", "Take Off Field Length", "Climb Gradient Takeoff", "Climb Gradient Takeoff 1 OEI", "Climb Gradient Cruise", "Climb Gradient Cruise 1 OEI", "Climb Gradient Landing", "Climb Gradient Landing 1 OEI", "Climb Rate"])
+plt.show()
