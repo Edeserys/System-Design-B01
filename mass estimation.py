ea66cbeb5ba0669ec825a8cf92cb25ba6208d89d
Edeserys <vikinikolov@yahoo.com>
2024-09-10 14:57:20 +0300

    Update mass estimation.py

--- mass estimation.py
+++ mass estimation.py
@@ -9,12 +9,15 @@
 # Drag
 # Thrust
 # Mass Estimation:
-    # Ranges
-    # Masses
+   # Masses with ADSEE I Book Method
+      # Mass Fractions
     # Mass Fractions
 
 # Import Libraries:
 import math
+import os
+
+os.system('cls')
 
 # Constants:
 pi = math.pi
@@ -22,6 +25,29 @@ euler = math.e
 gamma = 1.4 
 R = 287
 g = 9.81 # m/s^2
+
+def check_Mass_Fractions(f_OE, f_Fuel, f_payload):
+   print("\nMessages:")
+   if (f_OE > 0.6):
+      print("Operational Empty Mass is way too high!")
+   elif (0.5 <= f_OE <= 0.6):
+      print("Fuel Mass is within the bounds of 50 and 60 percent.")
+   elif (f_OE < 0.6):
+      print("Operational Empty Mass is way too little!")
+
+   if (f_Fuel > 0.3):
+      print("Fuel Mass is way too high!")
+   elif (0.2 <= f_Fuel <= 0.3):
+      print("Fuel Mass is within the bounds of 20 and 30 percent.")
+   elif (f_Fuel < 0.3):
+      print("Fuel Mass is way too little!")
+
+   if (f_payload > 0.3):
+      print("Payload Mass is way too high!")
+   elif (0.1 <= f_payload <= 0.3):
+      print("Payload Mass is within the bounds of 10 and 30 percent.")
+   elif (f_payload < 0.3):
+      print("Payload Mass is way too little!")
 
 # TLARs:
 # 1) Not Full Fuel
@@ -79,7 +105,8 @@ R_eq_design = (R_nom_design + R_lost) * (1 + f_con) + 1.2 * R_div + t_E * V_CR
 R_aux_zero = R_eq_zero - R_nom_zero
 R_aux_design = R_eq_design - R_nom_design
 
-# Masses:
+# 1) Masses with ADSEE Method:
+print("1) Masses with ADSEE I Book Method:")
 OE_Mass = maxMTOW_payload * (euler**((max_MTOW_R+R_aux_design)/(eta_j * L_over_D_CR * E_spec * 10**6/g))-1) / ((euler**((ZeroPayload_R+R_aux_zero)/(eta_j * L_over_D_CR * E_spec * 10**6/g))-euler**((max_MTOW_R+R_aux_design)/(eta_j * L_over_D_CR * E_spec * 10**6/g))))
 Fuel_Mass = OE_Mass * (euler**((max_MTOW_R+R_aux_design)/(eta_j * L_over_D_CR * E_spec * 10**6/g))-1) + max_payload*(euler**((max_MTOW_R+R_aux_design)/(eta_j * L_over_D_CR * E_spec * 10**6/g)))
 MTOW = maxMTOW_payload + Fuel_Mass + OE_Mass
@@ -89,38 +116,22 @@ f_FuelMass = Fuel_Mass / MTOW
 f_OEMass = OE_Mass / MTOW
 f_payload = maxMTOW_payload / MTOW
 
+# Print and Check Results:
 print("Fuel Mass Fraction: " + f"{f_FuelMass:.3f}" + "\nOperational Empty Mass Fraction: " + f"{f_OEMass:.3f}" + "\nPayload Mass Fraction: " + f"{f_payload:.3f}")
+check_Mass_Fractions(f_OEMass, f_FuelMass, f_payload)
 
-sw = []
-print("\nMessages:")
-if (f_OEMass > 0.6):
-   print("Operational Empty Mass is way too high!")
-elif (0.5 <= f_OEMass <= 0.6):
-    print("Fuel Mass is within the bounds of 50 and 60 percent.")
-    sw = sw.append(False)
-elif (f_OEMass < 0.6):
-   print("Operational Empty Mass is way too little!")
-
-if (f_FuelMass > 0.3):
-   print("Fuel Mass is way too high!")
-elif (0.2 <= f_FuelMass <= 0.3):
-    print("Fuel Mass is within the bounds of 20 and 30 percent.")
-    sw = sw.append(False)
-elif (f_FuelMass < 0.3):
-   print("Fuel Mass is way too little!")
-
-if (f_payload > 0.3):
-   print("Payload Mass is way too high!")
-elif (0.1 <= f_payload <= 0.3):
-    print("Payload Mass is within the bounds of 10 and 30 percent.")
-    sw = sw.append(False)
-elif (f_payload < 0.3):
-   print("Payload Mass is way too little!")
-
+# 2) Masses with Regression Analysis:
+print("\n2) Regression Analysis:")
 f_no_fuel = 1 - f_FuelMass
 MTOW = (OE_Mass_regression + maxMTOW_payload)/f_no_fuel
-Fuel_Mass = MTOW * f_FuelMass
-print("\nOperational Empty Weight:", OE_Mass_regression, "kg")
-print("Design Payload Mass:", maxMTOW_payload , "kg")
-print("Fuel Mass:", Fuel_Mass, "kg")
-print("Maximum Take-Off Weight:", MTOW, "kg")
+Fuel_Mass_reg = MTOW * f_FuelMass
+f_OE_reg = OE_Mass_regression / MTOW
+f_Fuel_reg = Fuel_Mass_reg / MTOW
+f_payload_reg = maxMTOW_payload / MTOW
+
+# Print and Check Results:
+print("Operational Empty Mass:", OE_Mass_regression, "kg -", f"{f_OE_reg:.3f}")
+print("Design Payload Mass:", maxMTOW_payload , "kg -", f"{f_payload_reg:.3f}")
+print("Fuel Mass:", f"{Fuel_Mass_reg:.3f}", "kg -", f"{f_Fuel_reg:.3f}")
+print("Maximum Take-Off Weight:", f"{MTOW:.3f}", "kg")
+check_Mass_Fractions(f_OE_reg, f_Fuel_reg, f_payload_reg)
