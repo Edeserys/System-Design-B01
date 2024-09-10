23397ccffcbf35757aa92387a5d1e226f08d6473
Daviyoshi <105122603+Daviyoshi@users.noreply.github.com>
2024-09-10 16:59:40 +0200

    Update mass estimation.py

--- mass estimation.py
+++ mass estimation.py
@@ -53,20 +53,7 @@ def print_Results(OE, f_OE, payload, f_payload, Fuel, f_Fuel, MTOW):
    print("Maximum Take-Off Weight:", f"{MTOW:.3f}", "kg")
 
 # TLARs:
-# 1) Not Full Fuel
-max_payload = 9302 # kg
-M = 0.77 
-alt_CR_ft = 35000 # ft
-alt_CR = alt_CR_ft * 0.3048 # m
-dist_TO = 1296 # m.
-dist_L =  1210 # m
-Design_R = 2019 * 1000 # m 
-Design_payload = 7200 # kg
-
-# 2) Full Fuel
-max_MTOW_R = 2574 * 1000 # m 
-maxMTOW_payload = 6355 # kg
-ZeroPayload_R = 2963 * 1000 # m
+from tlars import *
 
 # Assumptions:
 from assumptions import *
