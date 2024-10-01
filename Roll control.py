197a62745a997343d81eb492fb9a2cb8bc9cab0e
RHaanstra <R.A.Haanstra@student.tudelft.nl>
2024-10-01 13:52:11 +0200

    Roll control update

--- Roll control.py
+++ Roll control.py
@@ -1,16 +1,16 @@
 import math
 import matplotlib.pyplot as plt
 import numpy as np
-from SYSTEM-Design-B01.WP1.mass_estimation import *
-from constants.constants import *
+#from mass_estimation import *
+from constants import *
 
 
 
 
 
-c_lda = (c_root/2*y_aileronend**2 - (c_root*(1-taper_ratio)*y_aileronend**3)/(3*b/2)- (c_root/2*y_aileronstart**2 - (c_root*(1-taper_ratio)*y_aileronstart**3)/(3*b/2)))*(2*C_la*tau)/(S_ref*b)
+c_lda = (c_root/2*y_aileronend**2 - (c_root*(1-taper_ratio)*y_aileronend**3)/(3*b/2)- (c_root/2*y_aileronstart**2 - (c_root*(1-taper_ratio)*y_aileronstart**3)/(3*b/2)))*(2*C_la*tau)/(S_ref_ail*b)
 
-c_lp = -(4*(C_la +C_D0_CR))/(S_ref*b**2)* (c_root/3*(b/2)**3 - (c_root*(1-taper_ratio)*(b/2)**4)/(4*b/2))
+c_lp = -(4*(C_la +c_d0))/(S_ref_ail*b**2)* (c_root/3*(b/2)**3 - (c_root*(1-taper_ratio)*(b/2)**4)/(4*b/2))
 
 P = -(c_lda)/(c_lp)*da_max*(2*V_CR/b)
 
